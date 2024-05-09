ec6125cae776682b84517a4e48a49a223144f90c
David <67195057+david11133@users.noreply.github.com>
2024-05-09 21:33:05 +0300

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,39 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/I8hZjF
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/I8hZjF
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+DROP TABLE IF EXISTS "confirmed";
+DROP TABLE IF EXISTS "recovered";
+DROP TABLE IF EXISTS "death";
+
+CREATE TABLE "confirmed" (
+    "country" VARCHAR   NOT NULL,
+    "date" DATE   NOT NULL,
+    "accumulated_confirmed" INT   NOT NULL,
+    "increase_of_confirmed" INT   NOT NULL
+);
+
+CREATE TABLE "recovered" (
+    "country" VARCHAR   NOT NULL,
+    "date" DATE   NOT NULL,
+    "accumulated_recovered" INT   NOT NULL,
+    "increase_of_recovered" INT   NOT NULL
+);
+
+CREATE TABLE "death" (
+    "country" VARCHAR   NOT NULL,
+    "date" DATE   NOT NULL,
+    "accumulated_deaths" INT   NOT NULL,
+    "increase_of_deaths" INT   NOT NULL
+);
+
+-- Sanity checks post loading data from the jupyter notebook
+SELECT COUNT(*) from recovered;
+SELECT COUNT(*) FROM death;
+SELECT COUNT(*) FROM confirmed;
+
+
